3c1952fca739efb0c0b807bbdbd0cbe111d7e326
0521kimyi <0521kimyi@gmail.com>
2023-05-30 14:51:39 +0900

    -= 연산 추가, * 연산 추가

--- length.py
+++ length.py
@@ -32,39 +32,56 @@ class Length:
         self.value = self.value * (Length.standards[self.unit] / Length.standards[newunit])
         self.unit = newunit
     
+    # 수업 실습 : + 연산 (a+b 형식, a+3 형식)
     def __add__(self, other): 
-        if type(other) == int:
+        if type(other) == int:  # 숫자를 더해도 된다
             x = self.value + other
         else:
             x = self.value + other.value * (Length.standards[other.unit] / Length.standards[self.unit])
         return Length(x, self.unit)
-    
+    # 수업 실습 : += 연산
     def __iadd__(self, other):
         if type(other) == int:
             self.value += other
         else:
             self.value += other.value * (Length.standards[other.unit] / Length.standards[self.unit])
         return self
-    
+    # 수업실습 : + 연산 (3+a 형식)
     def __radd__(self, other):
         return self + other
     
-    # - 연산 (a-b 형식)
+    # 과제 : - 연산 (a-b 형식, 10-a 형식)
     def __sub__(self, other):
         if type(other) == int:
             x = self.value - other
         else:
             x = self.value - other.value * (Length.standards[other.unit] / Length.standards[self.unit])
         return Length(x, self.unit)
-    # - 연산 (10-a 형식)
+    # 과제 : - 연산 (10-a 형식)
     def __rsub__(self, other):
         if type(other) == int:
             x = other - self.value
         else:
             x = other.value - self.value * (Length.standards[self.unit] / Length.standards[other.unit])
         return Length(x, self.unit)
+    # 과제 : -= 연산
+    def __isub__(self, other):
+        if type(other) == int:
+            self.value -= other
+        else:
+            self.value -= other.value * (Length.standards[other.unit] / Length.standards[self.unit])
+        return self
     
-    
+    # 과제 : * 연산 (a*b 형식, a*3 형식)
+    def __mul__(self, other):
+        if type(other) == int:
+            x = self.value * other
+        else:
+            x = self.value * other.value
+        return Length(x, self.unit)
+    # 과제 : * 연산 (3*b 형식)
+    def __rmul__(self, other):
+        return self * other
         
 if __name__ == '__main__':
     a = Length(100, 'm')
@@ -74,28 +91,44 @@ if __name__ == '__main__':
     print('b = 20 ft = ', b)
     print('c = 50 cm = ', c)
     
-    #수업 실습
+    #수업 실습 : 단위 변환
     a.changeTo('mile')
     print('a = 100m (mile) =', a)
     b.changeTo('m')
     print('b = 20ft (m) =', b)
     
+    #수업 실습 : + 연산
     d = b + c
     print('d = b + c = ', d)
     d = b + 5
-    print('d = b + 5 = ', d) 
+    print('d = b + 5 = ', d)
+    d = 2 + b
+    print( 'd = 2 + b = ', d )
+    #수업 실습 : += 연산
     d += c
     print( 'd += c = ', d )
     d += 5
     print( 'd += 5 = ', d )
-    d = 2 + b
-    print( 'd = 2 + b = ', d )
     
-    # - 연산
+    
+    # 과제 : - 연산
     e = b - c
     print('e = b - c =', e)
-    e = b - 5
-    print('e = b - 5 =', e)
+    e = b - 3
+    print('e = b - 3 =', e)
     e = 10 - b
-    print('e = 8 - b =', e)
+    print('e = 10 - b =', e)
+    # 과제 : -= 연산
+    e -= c
+    print('e -= c = ', e)
+    e -= 2
+    print('e -= 2 = ', e)
+    
+    # 과제 : * 연산
+    f = b * c
+    print('f = b * c =', f)
+    f = b * 3
+    print('f = b * 3 =', f)
+    f = 3 * b
+    print('f = 3 * b =', f)
     
